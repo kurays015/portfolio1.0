22d5c900d81f21049398c30d08c486730ece7b02
Christzxz <christnarvarte15@gmail.com>
2023-09-16 20:52:27 +0800

    fix about grammars

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
           <hr />
           <p className="text-gray-400 py-3 ">
             Hi, I'm Christ Narvarte, a passionate, dedicated, and aspiring
-            Junior Frontend Developer with a love for coding and a knack for
-            creating innovative projects. As a Junior Developer, I am
+            junior frontend developer with a love for coding and a knack for
+            creating innovative projects. As a junior developer, I am
             continually inspired to explore and learn within the vast and
-            ever-evolving world of technology. My honed skills, shaped through
+            ever-evolving world of technology. My honed skills, shaped through a
             tremendous amount of effort and practice, fuel my journey towards
             becoming a more proficient and effective coder.
           </p>
@@ -26,7 +26,7 @@ const About = () => {
           </h1>
           <hr />
           <p className="text-gray-400 py-3">
-            My Journey as a developer began with a curiosity for turning ideas
+            My journey as a developer began with a curiosity for turning ideas
             into reality. This curiosity ignited my passion for coding and drove
             me to dive headfirst into the world of web development. Since then,
             I've been on an exciting learning adventure, soaking up knowledge
